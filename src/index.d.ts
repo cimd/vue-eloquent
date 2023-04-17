3930450c0e4f76ad80ca93f54e509207764b52fe
Christian D'Aquino <christian.daquino@gmail.com>
2023-04-17 20:30:52 +0800

    d

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import {formatObject} from './helpers/formatObject'
+import handleErrors from './helpers/handleErrors'
+import {http} from './http/http'
+
+export interface Api {
+  resource: string
+}
+
+export interface Model {
+  model: any
+}
+
+export interface Collection {
+  data: any[]
+}
