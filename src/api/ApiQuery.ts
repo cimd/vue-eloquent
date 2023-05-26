43b82354807131e0c81beb8be71d6665fd36774b
Christian D'Aquino <christian.daquino@gmail.com>
2023-05-26 11:15:53 +0800

    Create ApiQuery.ts

---
+++ src/api/ApiQuery.ts
@@ -0,0 +1,80 @@
+import { reactive } from 'vue'
+import type { IQuery } from '../collection/IQuery'
+import type { IQueryPage } from '../collection/IQueryPage'
+
+export default abstract class ApiQuery {
+
+  /**
+   * Filters used on GET request
+   */
+  protected filter: any = reactive({})
+  /**
+   * Relations used on GET request
+   */
+  protected include: string[] = reactive([])
+  /**
+   * Fields to requested through API
+   */
+  protected fieldsSelection: string[] = reactive([])
+  /**
+   * Pagination used on GET request
+   */
+  protected paging: IQueryPage = reactive({ })
+  /**
+   * Sorting used on GET request
+   */
+  protected sorting: string[] = reactive([])
+
+  protected constructor()
+  {
+  }
+
+  public where(filter: any): this
+  {
+    Object.assign(this.filter, filter)
+    return this
+  }
+
+  public with(relationships: string[]): this
+  {
+    this.include = [...relationships]
+    return this
+  }
+  public select(fields: string[]): this
+  {
+    this.fieldsSelection = [...fields]
+    return this
+  }
+  public sort(sorting: string[]): this
+  {
+    this.sorting = [...sorting]
+    return this
+  }
+  public page(paging: IQueryPage): this
+  {
+    Object.assign(this.paging, paging)
+    return this
+  }
+
+  protected queryString(): IQuery
+  {
+    const qs: IQuery = {}
+    if (this.filter) {
+      qs.filter = this.filter
+    }
+    if (this.include.length) {
+      qs.include = this.include.join(',')
+    }
+    if (this.fieldsSelection.length) {
+      qs.fields = this.fieldsSelection.join(',')
+    }
+    if (this.sorting.length) {
+      qs.sort = this.sorting.join(',')
+    }
+    if (this.paging) {
+      qs.page = this.paging
+    }
+
+    return qs
+  }
+}
