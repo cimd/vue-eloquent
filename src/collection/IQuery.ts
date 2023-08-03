7d9ebfd37d832a23738d745a6c72ab9af629ed69
Christian D'Aquino <christian.daquino@gmail.com>
2023-08-03 22:29:49 +0800

    Adding appends

--- src/collection/IQuery.ts
+++ src/collection/IQuery.ts
@@ -3,6 +3,7 @@ import type { IQueryPage } from '../collection/IQueryPage'
 export interface IQuery {
     fields?: string,
     include?: string,
+    append?: string,
     filter?: any[],
     page?: IQueryPage,
     sort?: string
