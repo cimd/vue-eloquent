53b7d01307872059cfacd5fa4b454017e7d9a35b
Christian D'Aquino <christian.daquino@gmail.com>
2023-08-28 20:02:30 +0800

    lint

--- src/devtools/devtools.ts
+++ src/devtools/devtools.ts
@@ -5,7 +5,7 @@ import { useModelInspector } from '../model/modelInspector'
 const inspectorId = 'vue-eloquent-devtools-plugin'
 let API: any
 
-export function setupDevtools(app: App) {
+export function setupDevtools (app: App) {
   // @ts-ignore
   setupDevtoolsPlugin({
     id: inspectorId,
@@ -14,19 +14,19 @@ export function setupDevtools(app: App) {
     homepage: 'https://vue-eloquent.netlify.app/',
     app
   },
-  api => {
-    API = api
-    console.log('🚀 Vue Eloquent DevTools Plugin installed')
+    api => {
+      API = api
+      console.log('🚀 Vue Eloquent DevTools Plugin installed')
 
-    api.addInspector({
-      id: inspectorId,
-      label: 'Vue Eloquent',
-      icon: 'api',
-    })
+      api.addInspector({
+        id: inspectorId,
+        label: 'Vue Eloquent',
+        icon: 'api',
+      })
 
-    api.on.getInspectorTree((payload, _context) => {
-      if (payload.inspectorId === inspectorId) {
-        payload.rootNodes =
+      api.on.getInspectorTree((payload, _context) => {
+        if (payload.inspectorId === inspectorId) {
+          payload.rootNodes =
             [
               {
                 id: 'Models',
@@ -38,50 +38,48 @@ export function setupDevtools(app: App) {
                 label: 'Collections'
               }
             ]
-      }
-    })
+        }
+      })
 
-    api.on.getInspectorState((payload, _context) => {
-      if (payload.inspectorId === inspectorId) {
-        if (payload.nodeId) {
-          // console.log(payload.nodeId)
-          const node = useModelInspector().childrenStates.find(el => el.id === payload.nodeId)
-          // console.log(node)
-          if (node === undefined) {
-            payload.state = {}
-            return
-          }
-          // console.log(node)
-          payload.state = {
-            'model': [
-              { key: 'uuid', value: node.model.uuid },
-              { key: 'model', value: node.model.model },
-              { key: 'defaultModel', value: node.model.defaultModel },
-              { key: 'relations', value: node.model.relations },
-            ],
-            'state': [
-              { key: 'isLoading', value: node.model.state.isLoading },
-              { key: 'isSuccess', value: node.model.state.isSuccess },
-              { key: 'isError', value: node.model.state.isError },
-            ],
-            'validation': [
-              { key: '$model', value: node.model.$model },
-              { key: '$invalid', value: node.model.$invalid },
-              { key: 'validations', value: node.model.validations },
-            ]
+      api.on.getInspectorState((payload, _context) => {
+        if (payload.inspectorId === inspectorId) {
+          if (payload.nodeId) {
+            const node = useModelInspector().childrenStates.find(el => el.id === payload.nodeId)
+            if (node === undefined) {
+              payload.state = {}
+              return
+            }
+
+            payload.state = {
+              'model': [
+                { key: 'uuid', value: node.model.uuid },
+                { key: 'model', value: node.model.model },
+                { key: 'defaultModel', value: node.model.defaultModel },
+                { key: 'relations', value: node.model.relations },
+              ],
+              'state': [
+                { key: 'isLoading', value: node.model.state.isLoading },
+                { key: 'isSuccess', value: node.model.state.isSuccess },
+                { key: 'isError', value: node.model.state.isError },
+              ],
+              'validation': [
+                { key: '$model', value: node.model.$model },
+                { key: '$invalid', value: node.model.$invalid },
+                { key: 'validations', value: node.model.validations },
+              ]
+            }
           }
         }
-      }
-    })
+      })
 
-    // api.on.inspectComponent((payload, context) => {
-    //   payload.instanceData.state.push({
-    //     type: stateType,
-    //     key: '$hello',
-    //     value: data.message
-    //   })
-    // })
-  })
+      // api.on.inspectComponent((payload, context) => {
+      //   payload.instanceData.state.push({
+      //     type: stateType,
+      //     key: '$hello',
+      //     value: data.message
+      //   })
+      // })
+    })
 }
 
 export const refreshInspector = async () => {
--- src/model/modelInspector.ts
+++ src/model/modelInspector.ts
@@ -8,7 +8,7 @@ export const childrenStates: any[] = []
 export const addModelInspector = async (model: Model) => {
   if (!model) throw new Error('Model is not defined')
 
-  console.log('Inspecting ', model.constructor.name)
+  // console.log('Inspecting ', model.constructor.name)
   eloquentModels.push(model)
   childrenNodes.push({
     id: model.uuid,
@@ -18,7 +18,17 @@ export const addModelInspector = async (model: Model) => {
     id: model.uuid,
     model: model
   })
-  // console.log(childrenNodes)
+
+  // TODO: Should the model be removed from the inspector?
+  //  It doesn't help debugging...
+  // onBeforeUnmount(async () => {
+  //   console.log('Removing model from inspector: ', model.uuid)
+  //   eloquentModels = eloquentModels.filter(element => element.uuid !== model.uuid)
+  //   childrenNodes = childrenNodes.filter(element => element.id !== model.uuid)
+  //   childrenStates = childrenStates.filter(element => element.id !== model.uuid)
+  //   await refreshInspector()
+  // })
+
   await refreshInspector()
 }
 
