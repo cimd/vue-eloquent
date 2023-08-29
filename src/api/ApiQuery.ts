055ff23c140c7d2d9dcd6027ce23199a50d1ec8a
Christian D'Aquino <christian.daquino@gmail.com>
2023-08-29 22:01:22 +0800

    Update ApiQuery.ts

--- src/api/ApiQuery.ts
+++ src/api/ApiQuery.ts
@@ -35,43 +35,88 @@ export default abstract class ApiQuery {
     return
   }
 
-  public where(filter: any)
+  /**
+   * Add a where clause to the query
+   *
+   * @param {object} filter - The filter to apply to the query
+   * @returns {this} The query instance
+   */
+  where(filter: any): this
   {
     Object.assign(this.filter, filter)
     refreshInspector().then()
     return this
   }
 
+  /**
+   * Add a where clause to the query
+   *
+   * @param {object} filter - The filter to apply to the query
+   */
   static where(filter: any)
   {
     const self = this.instance()
     return self.where(filter)
   }
 
+  /**
+   * Add relationships to the query
+   *
+   * @param {string[]} relationships - The relationships to include in the query
+   * @returns {this} The query instance
+   */
   with(relationships: string[]): this
   {
     this.include = [...relationships]
     refreshInspector().then()
     return this
   }
+
+  /**
+   * Add attributes to the query
+   *
+   * @param {string[]} attributes - The attributes to append to the query
+   * @returns {this} The query instance
+   */
   append(attributes: string[]): this
   {
     this.attributes = [...attributes]
     refreshInspector().then()
     return this
   }
+
+  /**
+   * Select specific fields to be returned by the query
+   *
+   * @param {string[]} fields - The fields to select
+   * @returns {this} The query instance
+   */
   select(fields: string[]): this
   {
     this.fieldsSelection = [...fields]
     refreshInspector().then()
     return this
   }
+
+  /**
+   * Sort the results of the query
+   *
+   * @param {string[]} sorting - The sorting criteria
+   * @returns {this} The query instance
+   */
   sort(sorting: string[]): this
   {
     this.sorting = [...sorting]
     refreshInspector().then()
     return this
   }
+
+  /**
+   * Set the pagination options for the query
+   *
+   * @param {object} paging - The pagination options
+   * @returns {this} The query instance
+   */
   page(paging: IQueryPage): this
   {
     Object.assign(this.paging, paging)
@@ -79,6 +124,11 @@ export default abstract class ApiQuery {
     return this
   }
 
+  /**
+   * Get the query parameters as a query string
+   *
+   * @returns {object} The query parameters
+   */
   protected queryString(): IQuery
   {
     const qs: IQuery = {}
