c779751f5b05d98c0b193461448c9ac9aacd5cc1
Christian D'Aquino <christian.daquino@gmail.com>
2024-05-10 18:17:08 +0800

    Update http.ts

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,3 +1,5 @@
+import { AxiosInstance } from 'axios'
+
 let http: any
 let apiPrefix: string = 'api'
 
@@ -5,10 +7,10 @@ let apiPrefix: string = 'api'
  * Create an instance of the HTTP service.
  *
  * @param { Object } config The configuration
- * @param { any } [config.httpClient] AxiosInstance
+ * @param { AxiosInstance } [config.httpClient] AxiosInstance
  * @param { string } [config.apiPrefix='/api'] The API prefix
  */
-function createHttp(config: { httpClient: any, apiPrefix?: string }): void
+function createHttp(config: { httpClient: AxiosInstance, apiPrefix?: string }): void
 {
   http = config.httpClient
   if (config.apiPrefix) apiPrefix = config.apiPrefix
