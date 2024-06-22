a6bd3bfe8d38dca82f6d3d274c812f468593ef93
Christian D'Aquino <christian.daquino@gmail.com>
2024-06-22 19:34:46 +0800

    d

---
+++ src/helpers/objects/diff.ts
@@ -0,0 +1,18 @@
+/**
+ * Returns the differencess between two objects
+ */
+const diff = (original: any, modified: any) => {
+  const changes = {}
+  for (const key in original) {
+    if (original.hasOwnProperty(key)) {
+      const val1 = original[ key ]
+      const val2 = modified[ key ]
+      if (val1 !== val2) {
+        changes[ key ] = [val1, val2]
+      }
+    }
+  }
+  return changes
+}
+
+export default diff
