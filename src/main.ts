750a83e772e4f8f3d3796bedcb067a27d3e9203c
Christian D'Aquino <christian.daquino@gmail.com>
2024-12-15 15:35:12 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import { PiniaApiPlugin } from './index'
 
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
-
-// give the plugin to pinia
-pinia.use(PiniaApiPlugin)
 
 app.mount('#app')
